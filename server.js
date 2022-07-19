c7b74546615fccd39b818f8aef432ef90fdcf536
yogeshparmar14 <mr.yoraj@gmail.com>
2022-07-19 23:20:49 +0530

    intail commit

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ const connectDb=require("./src/db/connectiondb.js");
 const userRoutes =require("./src/routes/userRoutes.js");
 const adminRoutes =require("./src/routes/adminRoutes.js");
 const imageRoutes =require("./src/modules/common/uploadFile.js")
+const path = require('path')
 
 
 const app = express();
 const port = process.env.PORT;
 const DATABASE_URL_LOCAL =process.env.DATABASE_URL_LOCAL
 const DATABASE_URL_ATLAS =process.env.DATABASE_URL_ATLAS
+
+app.set("view engine","hbs")
+app.set("views","./view")
+app.use(express.static(__dirname + "/public"))
+
 
 //cors policy
 app.use(cors());
@@ -26,6 +32,10 @@ connectDb(DATABASE_URL_LOCAL, DATABASE_URL_ATLAS);
 app.use("/user",userRoutes)
 app.use("/admin",adminRoutes)
 app.use("/common",imageRoutes)
+
+app.get("/",(req,res)=>{
+     res.render("index")
+})
 
 app.listen(port,()=>{
     console.log(`Server listening at http://localhost:${port}`)
